fa7e30450ca91388d93b074efe78ddd35416a597
crypt0box <pray.happy76@gmail.com>
2020-03-28 01:17:32 +0900

    追加：最初のコミット

---
+++ dotinstall/JavaScriptBassics/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+コメント
+コメント
+コメント
+*/
+    
+console.log('Hello World from main.js');
+console.log("it's me!");
+console.log('it\'s me!');
+
+console.log('hell\no wor\tld'); // \n:改行 \t:タブ
+
+console.log('hello' + 'world');
