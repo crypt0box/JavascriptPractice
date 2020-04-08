14fad0e58d64c949c41cef7076c02f5cec396708
crypt0box <pray.happy76@gmail.com>
2020-04-09 01:58:56 +0900

    initial commit

--- dotinstall/dotinstall_dom02/main.js
+++ dotinstall/dotinstall_dom02/main.js
@@ -2,7 +2,9 @@
 
 {
     function update() {
-        document.querySelector('h1').textContent = 'Changed!';
+        // document.querySelector('h1').textContent = 'Changed!';
+        // document.querySelector('#target').textContent = 'Changed!';
+        document.getElementById('target').textContent = 'Changed!';
     }
     
     setTimeout(update, 1000);
