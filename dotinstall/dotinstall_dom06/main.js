5d63aa8b5bd69579993c90167998e9510026a389
crypt0box <pray.happy76@gmail.com>
2020-04-09 21:51:19 +0900

    initial commit

---
+++ dotinstall/dotinstall_dom06/main.js
@@ -0,0 +1,11 @@
+`use strict`;
+
+{
+    document.querySelector('button').addEventListener('click', () => {
+        const targetNode = document.getElementById('target');
+        targetNode.textContent = 'Changed!';
+        targetNode.title = 'This is title!';
+        targetNode.style.color = 'red';
+        targetNode.style.backgroundColor = 'skyblue';
+    });
+}
