bb42425339a771cce197aae54eadc1fb5b3cd4bf
crypt0box <pray.happy76@gmail.com>
2020-04-09 21:58:17 +0900

    initial commit

---
+++ dotinstall/dotinstall_dom07/main.js
@@ -0,0 +1,10 @@
+`use strict`;
+
+{
+    document.querySelector('button').addEventListener('click', () => {
+        const targetNode = document.getElementById('target');
+
+        // targetNode.className = 'my-color';
+        targetNode.className = 'my-color my-border';
+    });
+}
