c1e62c4190560742c011597d12071db815d0e0cb
crypt0box <pray.happy76@gmail.com>
2020-04-12 19:20:59 +0900

    initial commit

---
+++ dotinstall/dotinstall_dom08/main.js
@@ -0,0 +1,17 @@
+`use strict`;
+
+{
+    document.querySelector('button').addEventListener('click', () => {
+        const targetNode = document.getElementById('target');
+
+        // targetNode.className = 'my-color';
+        // targetNode.className = 'my-color my-border';
+        // targetNode.classList.add('my-color');
+        // if (targetNode.classList.contains('my-color') == true) {
+        //     targetNode.classList.remove('my-color');
+        // } else {
+        //     targetNode.classList.add('my-color');
+        // }
+        targetNode.classList.toggle('my-color');
+    });
+}
