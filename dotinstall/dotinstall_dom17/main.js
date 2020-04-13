d125d5752dd797d2da97d522797cb6fa750e67b1
crypt0box <pray.happy76@gmail.com>
2020-04-13 16:16:04 +0900

    initial commit

---
+++ dotinstall/dotinstall_dom17/main.js
@@ -0,0 +1,11 @@
+`use strict`;
+
+{
+    document.querySelector('button').addEventListener('dblclick', () => {
+        console.log('Double Clicked!');
+    })
+
+    document.addEventListener('mousemove', () => {
+        console.log('moved!');
+    })
+}
