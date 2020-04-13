37765e7f5dfbf9cc05d6e5d5735ea0c754dbbaa6
crypt0box <pray.happy76@gmail.com>
2020-04-13 16:11:30 +0900

    initial commit

---
+++ dotinstall/dotinstall_dom16/main.js
@@ -0,0 +1,19 @@
+`use strict`;
+
+{
+    document.querySelector('button').addEventListener('click', () => {
+        const colors = document.querySelectorAll('input');
+        const selectedColors = [];
+
+        colors.forEach(color => {
+             if (color.checked === true) {
+                 selectedColors.push(color.value);
+             }
+        });
+
+        const li = document.createElement('li');
+        // li.textContent = selectedColors.join(',');
+        li.textContent = selectedColors;
+        document.querySelector('ul').appendChild(li);
+    });
+}
