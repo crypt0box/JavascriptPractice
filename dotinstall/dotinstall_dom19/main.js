fbf6feb457462e54d92e68abfc2db6ec048b83e2
crypt0box <pray.happy76@gmail.com>
2020-04-13 16:24:49 +0900

    initial commit

---
+++ dotinstall/dotinstall_dom19/main.js
@@ -0,0 +1,22 @@
+`use strict`;
+
+{
+    const text = document.querySelector('textarea');
+
+    // text.addEventListener('focus', () => {
+    //     console.log('focus');
+    // });
+
+    // text.addEventListener('blur', () => {
+    //     console.log('blur');
+    // });
+
+    text.addEventListener('input', () => {
+        // console.log('input');
+        console.log(text.value.length);
+    });
+
+    text.addEventListener('change', () => {
+        console.log('change');
+    });
+}
