9516506a281cfcd73d5917441f6103510480d878
crypt0box <pray.happy76@gmail.com>
2020-04-20 21:35:25 +0900

    initial commit

--- vuejs/template2/js/main.js
+++ vuejs/template2/js/main.js
@@ -5,10 +5,16 @@
     el: '#app',
     data: {
       number: 0,
+      x: 0,
+      y: 0,
     },
     methods: {
       countUp: function() {
         this.number += 1
+      },
+      changeMousePosition: function(event) {
+        this.x = event.clientX;
+        this.y = event.clientY;
       }
     }
   });
