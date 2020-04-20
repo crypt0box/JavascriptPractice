810f966b9bbeba63f3af59b78ed3636ee96a2677
crypt0box <pray.happy76@gmail.com>
2020-04-20 15:23:39 +0900

    initial commit

---
+++ vuejs/8/js/main.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      message: 'HelloWorld!',
+      number: 3,
+      ok: true,
+    },
+    methods: {
+      sayHi: function() {
+        return 'Hi';
+      }
+    }
+  });
+})();
