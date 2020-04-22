7e8da2e5a0fca3a81d002a80ae32309ef62864a7
crypt0box <pray.happy76@gmail.com>
2020-04-22 13:17:17 +0900

    引数[]

--- vuejs/template3/js/main.js
+++ vuejs/template3/js/main.js
@@ -6,20 +6,12 @@
     el: '#app',
     data: {
       number: 0,
-      x: 0,
-      y: 0,
+      event: 'click'
     },
     methods: {
       countUp: function() {
         this.number += 1
       },
-      changeMousePosition: function(event, divideNumber) {
-        this.x = event.clientX / divideNumber;
-        this.y = event.clientY / divideNumber;
-      },
-      myAlert() {
-        alert("アラート！")
-      }
     }
   });
 })();
