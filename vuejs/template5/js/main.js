d3cc2f8001db834d19451f3ef8606703a186bcd6
crypt0box <pray.happy76@gmail.com>
2020-04-22 16:02:25 +0900

    スタイル

---
+++ vuejs/template5/js/main.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      isActive: true
+    },
+    computed: {
+      classObject: function() {
+        return {
+          red: this.isActive,
+          'bg-blue': !this.isActive
+        }
+      }
+    },
+  });
+})();
