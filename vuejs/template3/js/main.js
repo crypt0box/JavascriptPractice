140e6d5fe890aaa5491f808821f8825df4b4ff66
crypt0box <pray.happy76@gmail.com>
2020-04-22 13:35:57 +0900

    computedプロパティ

--- vuejs/template3/js/main.js
+++ vuejs/template3/js/main.js
@@ -6,12 +6,18 @@
     el: '#app',
     data: {
       number: 0,
-      event: 'click'
+      counter: 0,
+      event: 'click',
     },
     methods: {
       countUp: function() {
         this.number += 1
       },
-    }
+    },
+    computed: {
+      lessThanThree: function() {
+        return this.counter > 3 ? '3より上' : '3以下'
+      },
+    },
   });
 })();
