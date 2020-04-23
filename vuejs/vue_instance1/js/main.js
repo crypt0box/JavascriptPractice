a76f18034001de56219019975c07de7c5719ab80
crypt0box <pray.happy76@gmail.com>
2020-04-23 13:03:31 +0900

    initial commit

---
+++ vuejs/vue_instance1/js/main.js
@@ -0,0 +1,18 @@
+var vm1 = new Vue({
+  el: '#app1',
+  data: {
+    message: 'インスタンス1'
+  }
+});
+
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    message: 'インスタンス2'
+  },
+  methods: {
+    changeMessage1: function() {
+      vm1.message = 'インスタンス2から変えました'
+    }
+  }
+});
