048db757f1cc64875e9fa2d1b4c65dacc1a3564c
crypt0box <pray.happy76@gmail.com>
2020-04-29 16:23:25 +0900

    initial commit

--- vuejs/udemy-vuejs/src/App.vue
+++ vuejs/udemy-vuejs/src/App.vue
@@ -15,6 +15,12 @@
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
+    <div>
+      <h2>イベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input id="title" type="text" v-model="eventData.title">
+      <p>{{ eventData.title }}</p>
+    </div>
   </div>
 </template>
 
@@ -27,7 +33,10 @@ export default {
   data() {
     return {
       number: 10,
-      currentComponent: "Home"
+      currentComponent: "Home",
+      eventData: {
+        title: "タイトル"
+      }
     };
   },
   components: {
@@ -37,9 +46,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-div {
-  border: 1px solid blue;
-}
-</style>
