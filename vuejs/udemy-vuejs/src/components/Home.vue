ab739883a2703cb7b682fe9ae61f2a9ceb2a20b3
crypt0box <pray.happy76@gmail.com>
2020-04-30 14:53:11 +0900

    initial commit

--- vuejs/udemy-vuejs/src/components/Home.vue
+++ vuejs/udemy-vuejs/src/components/Home.vue
@@ -1,3 +1,3 @@
 <template>
-  <p>Home</p>
+  <p v-border:dotted="{width: '5px', color: 'red'}">Home</p>
 </template>
--- vuejs/udemy-vuejs/src/main.js
+++ vuejs/udemy-vuejs/src/main.js
@@ -4,6 +4,27 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
+Vue.directive("border", function(el, binding){
+   // bind(el, binding, vnode) {
+   //    // ディレクティブが初めて対象の要素に紐づいた時
+   // },
+   // inserted(el, binding, vnode) {
+   //    // 親Nodeに挿入された時
+   // },
+   // update(el, binding, vnode, oldVnode) {
+   //    // コンポーネントが更新されるたび。子コンポーネントが更新される前
+   // },
+   // componentUpdated(el, binding, vnode, oldVnode) {
+   //    // コンポーネントが更新されるたび。子コンポーネントが更新された後
+   // },
+   // unbind(el, binding, vnode) {
+   //    // ディレクティブが紐づいている要素から取り除かれたとき
+   // },
+   // el.style.border = "solid black 2px";
+   el.style.borderWidth = binding.value.width;
+   el.style.borderColor = binding.value.color;
+   el.style.borderStyle = binding.arg;
+});
 
 new Vue({
    render: h => h(App),
