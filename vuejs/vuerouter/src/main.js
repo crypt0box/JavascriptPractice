4fa2d2e44bd6ab2489cbee1b6acc6353908a6028
crypt0box <pray.happy76@gmail.com>
2020-05-06 16:48:45 +0900

    initial commit

--- vuejs/vuerouter/src/main.js
+++ vuejs/vuerouter/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router"
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// router.beforeEach((to, from, next) => {
+//   next();
+// })
 
 new Vue({
   router: router,
