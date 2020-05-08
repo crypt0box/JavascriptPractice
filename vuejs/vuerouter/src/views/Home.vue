93a52661c7512c2c229cdf55e8848c63a919bc70
crypt0box <pray.happy76@gmail.com>
2020-05-08 19:44:25 +0900

    initial commit

--- vuejs/vuerouter/src/views/Home.vue
+++ vuejs/vuerouter/src/views/Home.vue
@@ -2,17 +2,25 @@
   <div>
     <h3>Home</h3>
     <button @click="toUsers">Usersのページに行く</button>
-    <p>{{ count }}</p>
+    <p>{{ doubleCount }}</p>
+    <p>{{ tripleCount }}</p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  computed: {
-    count() {
-      return this.$store.state.count;
-    }
-  },
+  computed: mapGetters(["doubleCount", "tripleCount"]),
+  // computed: {
+  //   doubleCount() {
+  //     // return this.$store.state.count;
+  //     return this.$store.getters.doubleCount;
+  //   },
+  //   tripleCount() {
+  //     return this.$store.getters.tripleCount;
+  //   },
+  // },
   methods: {
     toUsers() {
       this.$router.push('users/1');
