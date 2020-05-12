d0304cce8851120a37d3d04e16dbaf0195cee3a3
crypt0box <pray.happy76@gmail.com>
2020-05-12 18:33:13 +0900

    initial commit

--- vuejs/http-vuejs/src/main.js
+++ vuejs/http-vuejs/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import axios from "axios";
+import router from "./router";
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/http-vuejs-f7659/databases/(default)/documents";
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ vuejs/http-vuejs/src/views/Register.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>登録</h3>
+    <label for="email">Email</label>
+    <input id="email" type="email" v-model="email" />
+    <br /><br />
+    <label for="password">パスワード</label>
+    <textarea id="password" v-model="password"></textarea>
+    <br /><br />
+    <button @click="register">送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    register() {}
+  }
+};
+</script>
