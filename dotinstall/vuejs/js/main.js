84ff8c3ffd0277a469c7b3c74297d4e4bf47e265
crypt0box <pray.happy76@gmail.com>
2020-05-24 18:24:29 +0900

    initial commit

--- dotinstall/vuejs/js/main.js
+++ dotinstall/vuejs/js/main.js
@@ -6,7 +6,41 @@
   var vm = new Vue({
     el: '#app',
     data: {
-      name: 'taguchi'
+      name: 'taguchi',
+      newItem: '',
+      todos: [{
+        title: 'task 1',
+        isDone: false
+      }, {
+        title: 'task 2',
+        isDone: false
+      }, {
+        title: 'task 3',
+        isDone: true
+      }]
+    },
+    methods: {
+      addItem() {
+        var item = {
+          title: this.newItem,
+          isDone: false
+        }
+        this.todos.push(item)
+        this.newItem = ''
+      },
+      deleteItem(index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1)
+        }
+      }
+    },
+    computed: {
+      remaining() {
+        var items = this.todos.filter(function(todo) {
+          return !todo.isDone;
+        });
+        return items.length;
+      }
     }
   });
 })();
